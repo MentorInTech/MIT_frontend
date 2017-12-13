bb077d82e905766fd749cc0d44e02b3e8ee4cd36
Bruce Weng <bruce.tw.weng@foxconn.com>
2017-12-14 01:20:27 +0800

    install material-ui; add title and card in signup page

--- mit_frontend/src/App.js
+++ mit_frontend/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Card, CardTitle} from 'material-ui/Card';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="title">Mentor In Tech</p>
+        <MuiThemeProvider>
+          <Card className="sign-up-card">
+            <CardTitle title="Sign Up" className="card-title">
+
+            </CardTitle>
+          </Card>
+        </MuiThemeProvider>
       </div>
     );
   }
