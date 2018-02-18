1e3a622ae5223caae5c4dbb349cf2ad0f01352f5
David-Shih <shiliangdavid@126.com>
2018-02-18 11:35:06 -0800

    Add username and password empty check on login page

--- mit_frontend/src/routers/Login.js
+++ mit_frontend/src/routers/Login.js
@@ -17,9 +17,54 @@ class Login extends Component {
     super(props);
 
     this.state = {
-      userName: null,
-      password: null
+      userName: "",
+      password: "",
+      userNameErrorText: "",
+      passwordErrorText: "",
     };
+  }
+
+  /**
+   * Update input value to state.userName.
+   * When the inputvalue is empty, show the error message.
+   */
+  userNameOnChange(event) {
+    this.setState({userName: event.target.value});
+    if (this.state.userName !== "") {
+      this.setState({ userNameErrorText: ""});
+    }
+  }
+
+  /**
+   * Update input value to state.password.
+   * When the inputvalue is empty, show the error message.
+   */
+  passwordOnChange(event) {
+    this.setState({password: event.target.value});
+    if (this.state.password !== "") {
+      this.setState({ passwordErrorText: ""});
+    }
+  }
+
+  /**
+   * This function is called when sign up button clicked. Will check all the fields, if there
+   * is any empty fields in TextField, highlight the field.
+   */
+  onChange(event) {
+    console.log("clicked!");
+    if (this.state.userName === "") {
+      this.setState({ userNameErrorText: "This field is required"});
+    } else {
+      this.setState({ userNameErrorText: ""});
+    }
+
+    if (this.state.password === "") {
+      this.setState({ passwordErrorText: "This field is required"});
+    } else {
+      this.setState({ passwordErrorText: ""});
+    }
+
+
   }
 
   render() {
@@ -35,9 +80,9 @@ class Login extends Component {
               floatingLabelFocusStyle={styles.textField.text}
               underlineFocusStyle={styles.textField.underline}
               value={this.state.userName}
-              onChange={
-                (event) => {this.setState({userName: event.target.value})}
-              }
+              errorText={this.state.userNameErrorText}
+              maxLength="10"
+              onChange={ this.userNameOnChange.bind(this) }
             />
             <TextField
               hintText="Password"
@@ -46,9 +91,8 @@ class Login extends Component {
               floatingLabelFocusStyle={styles.textField.text}
               underlineFocusStyle={styles.textField.underline}
               value={this.state.password}
-              onChange={
-                (event) => {this.setState({password: event.target.value})}
-              }
+              errorText={this.state.passwordErrorText}
+              onChange = {this.passwordOnChange.bind(this)}
             />
             <br/>
             <br/>
@@ -56,6 +100,9 @@ class Login extends Component {
               label="Log In"
               labelColor="#ffffff"
               backgroundColor={styles.button.color}
+              onClick={
+                this.onChange.bind(this)
+              }
             />
           <Link to={`/forget-password`} style={{ textDecoration: 'none' }}>
               <p className="forget-password">Forget Password?</p>
