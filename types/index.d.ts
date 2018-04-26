80ba378448cbb88abc8cbe904737708a29827fae
Sam Ling <sam@thling.com>
2018-04-26 02:47:12 -0700

    Reimplement sign-in page with new design (#21)
    This change also:
    
     - Refactored inputs into its own component
     - Make navbar mobile friendly with hamburger menu under smaller screens

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,32 @@
-/* Page component */
-export interface PageProps {
+import * as React from 'react';
+
+export interface PageFrameProps {
+  children: any,
   title: string,
-  children: any
+  path?: string
 }
 
-export interface SignUpState {
+export interface NavbarProps {
+  path?: string
+}
+
+export interface NavbarStates {
+  burgerActive: boolean
+}
+
+export interface InputFieldProps {
+  disabled?: boolean,
+  helpMessage?: string,
+  inputClassName?: string,
+  layoutClassName?: string,
+  name: string,
+  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void,
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  placeholder?: string,
+  type: string
+}
+
+export interface SignUpStates {
   awaitServer: boolean,
   confirm: string,
   confirmBlurredOnce: boolean,
@@ -22,6 +44,19 @@ export interface SignUpState {
   lastNameBlurredOnce: boolean,
   lastNameMessage: string,
   lastNameValid: boolean,
+  passwordBlurredOnce: boolean,
+  password: string,
+  passwordMessage: string,
+  passwordValid: boolean,
+  readyForSubmit: boolean
+}
+
+export interface SignInStates {
+  awaitServer: boolean,
+  email: string,
+  emailBlurredOnce: boolean,
+  emailMessage: string,
+  emailValid: boolean,
   passwordBlurredOnce: boolean,
   password: string,
   passwordMessage: string,
